c7608f72ac4887516792b5d1128cffe7e03e67b8
Gagan Kumar <gagan1kumar.cs@gmail.com>
2018-07-14 22:13:40 +0530

    R3

--- tests/e2e/test_forum.py
+++ tests/e2e/test_forum.py
@@ -13,7 +13,7 @@ class TestForum(BaseTestCase):
         super().__init__(testname,user_id="instructor", user_password="instructor", user_name="Quinn")
 
     def init_and_enable_discussion(self):
-        self.driver.find_element_by_id(self.get_current_semester() + '+_sample').click()
+        self.driver.find_element_by_id(self.get_current_semester() + '_sample').click()
         if len(self.driver.find_elements_by_xpath("//a[contains(text(),'Discussion Forum')]")) == 0:
             self.driver.find_element_by_xpath("//a[contains(text(),'Course Settings')]").click()
             self.driver.find_element_by_name("forum_enabled").click()
